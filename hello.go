d1678c7b6adc0dc14c85ced110e826c7e09e887f
minbohe <hemin_it@163.com>
2018-05-08 23:10:51 +0800

    update

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// THis a test 
+	fmt.Println("Hello World 123")
+	var age int = 1000
+	fmt.Println(age + 1)
+	var isAction bool
+	isAction = true
+	fmt.Println(isAction)
+	var a = 1.55
+	var b = 10
+	fmt.Println(a,b)
+}
